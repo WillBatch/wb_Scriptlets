0b948556bb4288fc89bca04a3f22c85568396bc5
WillBatch <williamf.batchelor@gmail.com>
2024-04-02 10:01:51 -0700

    added more UI for controlling row label and row start and prefix

--- Layer-Media/Lower-Third-Batchmaker.jsx
+++ Layer-Media/Lower-Third-Batchmaker.jsx
@@ -1,4 +1,5 @@
-var lineToStartOn = 3;
+var rowWithLabelText = 1;
+var rowToStartOn = 3;
 var textSearchOne = "Line 1";
 var textSearchTwo = "Line 2";
 var compNamePrefix = "NW-ToA";
@@ -58,7 +59,13 @@ var compNamePrefix = "NW-ToA";
         //check for valid input from csv path
         if (filePathText.text !== "") {
           csvFile = new File(filePathText.text);
-          runMainScript(app.project.selection[0], parseCSV(csvFile));
+          runMainScript(
+            app.project.selection[0],
+            parseCSV(csvFile),
+            rowLabelsInputText.text,
+            startingLineTextInput.text,
+            prefixTextInput.text
+          );
         } else {
           alert("Please browse for CSV File");
         }
@@ -85,58 +92,34 @@ var compNamePrefix = "NW-ToA";
     var filePathText = group2.add("edittext", undefined, "", {
       multiline: false,
     });
-
+    // Add a text field for Row Labels
+    var rowLabelsStaticText = group3.add("statictext", undefined, "Label Row:");
+    var rowLabelsInputText = group3.add("edittext", undefined, "1", {
+      multiline: false,
+    });
     // Add a text field for Line Count Variable
     var startingLineLabel = group3.add(
       "statictext",
       undefined,
       "CSV Row Start:"
     );
-    var startingLineTextInput = group3.add("edittext", undefined, "1", {
+    var startingLineTextInput = group3.add("edittext", undefined, "2", {
       multiline: false,
     });
     // Add a text field for Prefix variable
-    var prefixLabel = group3.add("statictext", undefined, "Prefix");
-    var prefixTextInput = group3.add("edittext", undefined, "", {
+    var prefixLabel = group4.add("statictext", undefined, "Prefix");
+    var prefixTextInput = group4.add("edittext", undefined, "", {
       multiline: false,
     });
-    // Add a text field for Text Line One variable
-    var textOneSearchNameLabel = group4.add(
-      "statictext",
-      undefined,
-      "Column 1 Search Text"
-    );
-    var textOneSearchNameTextInput = group4.add(
-      "edittext",
-      undefined,
-      "Line 1",
-      {
-        multiline: false,
-      }
-    );
-    // Add a text field for Prefix variable
-    var textTwoSearchNameLabel = group5.add(
-      "statictext",
-      undefined,
-      "Column 2 Search Text"
-    );
-    var textTwoSearchNameTextInput = group5.add(
-      "edittext",
-      undefined,
-      "Line 2",
-      {
-        multiline: false,
-      }
-    );
 
     //Button Sizing
     button_runMainScript.size = [120, 20];
     browseButton.size = [85, 20];
     filePathText.size = [205, 20];
+    rowLabelsInputText.size = [30, 20];
+
     startingLineTextInput.size = [30, 20];
     prefixTextInput.size = [100, 20];
-    textOneSearchNameTextInput.size = [100, 20];
-    textTwoSearchNameTextInput.size = [100, 20];
 
     win.onResizing = win.onResize = function () {
       this.layout.resize();
@@ -164,26 +147,46 @@ function parseCSV(csvFile) {
   return lines;
 }
 
-function runMainScript(comp, lines) {
+function runMainScript(comp, lines, labelRow, startRow, prefix) {
   app.beginUndoGroup("Make Lower Thirds");
-  for (var i = lineToStartOn - 1; i < lines.length; i++) {
+  var rowOneSearchNames_Array = getRowSearchNames(
+    lines[parseInt(labelRow) - 1]
+  );
+  for (var i = parseInt(startRow) - 1; i < lines.length; i++) {
     var values = lines[i].split(",");
     var textInputOne = values[0]; // Assuming first column
     var textInputTwo = values[1]; // Assuming second column
-    make_lower_third(comp, textInputOne, textInputTwo);
+    make_lower_third(
+      comp,
+      textInputOne,
+      textInputTwo,
+      rowOneSearchNames_Array,
+      values,
+      prefix
+    );
   }
   app.endUndoGroup();
 }
-function make_lower_third(comp, textInputOne, textInputTwo) {
+function make_lower_third(
+  comp,
+  textInputOne,
+  textInputTwo,
+  rowOneSearchNames_Array,
+  values,
+  prefix
+) {
   var newComp = comp.duplicate();
-  var textOne = search_layer_by_name(newComp, textSearchOne);
-  var textTwo = search_layer_by_name(newComp, textSearchTwo);
+  for (var i = 0; i < rowOneSearchNames_Array.length; i++) {
+    var searchText = search_layer_by_name(
+      newComp,
+      rowOneSearchNames_Array[i].toString()
+    );
+    if (searchText) {
+      searchText.property("Text").property("Source Text").setValue(values[i]);
+    }
+  }
 
-  textOne.property("Text").property("Source Text").setValue(textInputOne);
-  textTwo.property("Text").property("Source Text").setValue(textInputTwo);
-
-  var newCompName =
-    compNamePrefix + "_" + "L3_" + textInputOne + "" + textInputTwo;
+  var newCompName = prefix + "_" + "L3_" + textInputOne + "" + textInputTwo;
   var newCompNameFormatted = newCompName.replace(/[<>:"\/\\|?*\s]/g, "");
 
   newComp.name = newCompNameFormatted;
@@ -197,4 +200,14 @@ function search_layer_by_name(comp, name) {
     }
   }
   return matchedLayer;
+}
+function getRowSearchNames(csv_row) {
+  var rowOneAll = csv_row.split(",");
+  var rowOne = [];
+  for (var i = 0; i < rowOneAll.length; i++) {
+    if (rowOneAll[i] !== "") {
+      rowOne.push(rowOneAll[i]);
+    }
+  }
+  return rowOne;
 }
