202fafbc08e7b78c430875cc2b3ca59f889382e0
WillBatch <williamf.batchelor@gmail.com>
2024-04-03 05:16:11 -0700

    new file

---
+++ executeCommand-ScratchPad.jsx
@@ -0,0 +1,8 @@
+// app.executeCommand(2702)
+
+// var cmdID = app.findMenuCommandId("Copy with Property Links"); //10310
+var cmdID = app.findMenuCommandId("Paste"); //20
+
+// alert(cmdID)
+
+// app.executeCommand(10310)
