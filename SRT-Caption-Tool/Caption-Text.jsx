f80337c4e146cff9fae02f6c67e992a46ec6b7c1
billybatch <williamf.batchelor@gmail.com>
2024-04-27 10:07:31 -0700

    updtaes

--- SRT-Caption-Tool/Caption-Text.jsx
+++ SRT-Caption-Tool/Caption-Text.jsx
@@ -1,51 +1,61 @@
 // Main script
-var mainComp = app.project.activeItem;
-if (mainComp && mainComp instanceof CompItem) {
-  var compositionsArray = collectCompositions(mainComp);
-
-  if (compositionsArray.length > 0) {
-    app.beginUndoGroup("Duplicate Text Layers");
-
-    duplicateTextLayers(mainComp, compositionsArray);
-    app.endUndoGroup();
-  } else {
-    alert("No nested compositions found in the main composition.");
-  }
-} else {
-  alert("Please select or open a composition in After Effects.");
-}
-
-// Function to collect all compositions within the main composition
-function collectCompositions(mainComp) {
-  var compositionsArray = [];
-
-  for (var i = 1; i <= mainComp.numLayers; i++) {
-    var layer = mainComp.layer(i);
-    if (layer.source instanceof CompItem) {
-      compositionsArray.push(layer);
+var selectedItems = selectedCompItems(app.project);
+if (selectedItems && app.project.activeItem) {
+  var selectedItems_Comps = [];
+  for (var i = 0; i < selectedItems.length; i++) {
+    if (isCompositionLayer(selectedItems[i])) {
+      selectedItems_Comps.push(selectedItems[i]);
     }
   }
-  return compositionsArray;
+  app.beginUndoGroup("Prep Text");
+  decomposeTextFromComps(selectedItems_Comps, app.project.activeItem);
+  app.endUndoGroup();
+  var promptDelete = confirm("Delete text compositions?"); // This line displays a confirmation dialog with a message and OK/Cancel buttons.
+  if (promptDelete) {
+    app.beginUndoGroup("Delete");
+    deleteSelectedItems_Comps(selectedItems_Comps);
+    app.endUndoGroup();
+  }
+} else {
+  alert("Please select the text compositions in the project panel.");
 }
 
-// Function to duplicate text layers from compositions and add them to the main composition
-function duplicateTextLayers(mainComp, compositionsArray) {
+function decomposeTextFromComps(compositionsArray, mainComp) {
   for (var i = 0; i < compositionsArray.length; i++) {
     var comp = compositionsArray[i];
-    comp.openInViewer();
     for (var j = 1; j <= comp.numLayers; j++) {
       var layer = comp.layer(j);
-
       if (layer instanceof TextLayer) {
+        var layerPosition = layer.property("Transform").property("Position");
+
+        var textProp = layer.property("Source Text");
+        var textDocument = textProp.value;
         var newTextLayer = mainComp.layers.addText(layer.sourceText.value);
+        newTextLayer.sourceText.setValue(textDocument);
         newTextLayer.startTime = layer.startTime;
         newTextLayer.inPoint = layer.inPoint;
         newTextLayer.outPoint = layer.outPoint;
+        newTextLayer
+          .property("Transform")
+          .property("Position")
+          .setValue(layerPosition.value);
       }
     }
   }
 }
+function deleteSelectedItems_Comps(comps) {
+  for (var i = comps.length - 1; i >= 0; i--) {
+    comps[i].remove();
+  }
+}
 
-// function isCompositionLayer(layer) {
-//   return layer.source instanceof CompItem;
+function selectedCompItems(project) {
+  return project.selection.length > 0 ? project.selection : null;
+}
+function isCompositionLayer(layer) {
+  // return layer.source instanceof CompItem;
+  return layer instanceof CompItem;
+}
+// function isTextLayer(layer) {
+//   return layer.source instanceof TextLayer;
 // }
